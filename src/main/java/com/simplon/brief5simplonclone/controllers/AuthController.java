bbf77d0d23af4e7e5e5d36a4277a253580697944
Said Oudouane <saidoudouane@gmail.com>
2022-11-08 23:34:41 +0100

    feat: add dependency injection capability

--- src/main/java/com/simplon/brief5simplonclone/controllers/AuthController.java
+++ src/main/java/com/simplon/brief5simplonclone/controllers/AuthController.java
@@ -2,20 +2,31 @@ package com.simplon.brief5simplonclone.controllers;
 
 import com.simplon.brief5simplonclone.annotations.Controller;
 import com.simplon.brief5simplonclone.annotations.Handler;
+import com.simplon.brief5simplonclone.annotations.Inject;
 import com.simplon.brief5simplonclone.annotations.Methods;
 import com.simplon.brief5simplonclone.annotations.Param;
 import com.simplon.brief5simplonclone.core.Response;
+import com.simplon.brief5simplonclone.entities.Brief;
+import com.simplon.brief5simplonclone.entities.User;
+import com.simplon.brief5simplonclone.services.Service;
 import jakarta.servlet.ServletException;
 import java.io.IOException;
 
 @Controller()
 public class AuthController {
 
+  @Inject
+  private Service<User> userService;
+
+  @Inject
+  private Service<Brief> briefService;
+
 
   @Handler(path = "/login/{name}")
   public void login(Response response, @Param("name") String name)
       throws IOException, ServletException {
-    response.send("hello there " + name);
+
+    response.send(new Object[]{userService.getAll(), briefService.getAll()});
   }
 
   @Handler(path = "/register", method = Methods.POST)
--- src/main/java/com/simplon/brief5simplonclone/utils/Dependency.java
+++ src/main/java/com/simplon/brief5simplonclone/utils/Dependency.java
@@ -1,13 +1,22 @@
 package com.simplon.brief5simplonclone.utils;
 
+import com.simplon.brief5simplonclone.annotations.Inject;
+import com.simplon.brief5simplonclone.services.Service;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Type;
+import java.util.HashMap;
 
 public class Dependency {
 
+  private static final HashMap<String, Object> instances = new HashMap<>();
 
   public static <T> T getInstance(Class<T> clazz) {
+    if (instances.containsKey(clazz.getName())) {
+      return (T) instances.get(clazz.getName());
+    }
     try {
       Constructor<?>[] constructors = clazz.getDeclaredConstructors();
       Constructor<?> constructor = null;
@@ -23,25 +32,47 @@ public class Dependency {
       Object[] params = new Object[constructor.getParameterCount()];
       for (int i = 0; i < constructor.getParameterCount(); i++) {
         Class<?> paramType = constructor.getParameterTypes()[i];
+        Type genericParameterType = constructor.getGenericParameterTypes()[i];
+        if (clazz == paramType) {
+          throw new RuntimeException("Circular dependency detected for class " + clazz.getName());
+        }
+        if (paramType == Service.class) {
+          params[i] = getService(genericParameterType.toString());
+          continue;
+        }
 
-        if (!paramType.isInterface() && paramType != Class.class) {
+        if (!paramType.isInterface()) {
           params[i] = getInstance(paramType);
         }
       }
       T instance = (T) constructor.newInstance(params);
-//      Field[] fields = clazz.getDeclaredFields();
-//      for (Field field : fields) {
-//        if (field.isAnnotationPresent(Inject.class)) {
-//          field.setAccessible(true);
-//          field.set(instance, getInstance(field.getType()));
-//          field.setAccessible(false);
-//        }
-//      }
+      Field[] fields = clazz.getDeclaredFields();
+      for (Field field : fields) {
+        if (field.isAnnotationPresent(Inject.class)) {
+          field.setAccessible(true);
+          if (field.getType() == Service.class) {
+            field.set(instance, getService(field.getGenericType().toString()));
+            continue;
+          }
+          field.set(instance, getInstance(field.getType()));
+          field.setAccessible(false);
+        }
+      }
+      instances.put(clazz.getName(), instance);
       return instance;
     } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
-             NoSuchMethodException e) {
+             NoSuchMethodException | ClassNotFoundException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  private static Service<?> getService(String type) throws ClassNotFoundException {
+    String genericParam = type.replaceAll(".*<(.*)>.*", "$1");
+    Class<?> EntityName = Class.forName(genericParam);
+    if (!instances.containsKey(EntityName.getName())) {
+      instances.put(EntityName.getName(), new Service(EntityName));
+    }
+    return (Service<?>) instances.get(EntityName.getName());
   }
 
 }
